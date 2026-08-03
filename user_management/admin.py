2ec1b98838c91c0ed76037025063bde73f24b804
Zahidul-Turja <zahidulturja@gmail.com>
2026-08-03 17:40:43 +0600

    Implement custom UserAdmin for user management with enhanced display and permissions

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,50 @@
+# user_management/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from user_management.models import User, InstructorApplication
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = [
+        "email",
+        "full_name",
+        "role",
+        "is_verified",
+        "is_active",
+        "is_staff",
+    ]
+    list_filter = ["role", "is_verified", "is_active", "auth_provider"]
+    search_fields = ["email", "full_name"]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("full_name", "role", "auth_provider")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "role"),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(InstructorApplication)
